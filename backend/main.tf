9586eadc1e5a2b9b06041705331c9d422178e446
Premkumar Nagaiah <138997281+PremN-IT@users.noreply.github.com>
2026-04-26 09:38:32 +0530

    Update S3 bucket configuration and remove DynamoDB table
    Added use_lockfile attribute to S3 bucket resource and removed DynamoDB table resource.

--- backend/main.tf
+++ backend/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "demo-terraform-eks-state-s3-bucket-test"
-
+  use_lockfile = "true"
   lifecycle {
     prevent_destroy = false
   }
@@ -24,16 +24,5 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-eks-state-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
   }
 }
