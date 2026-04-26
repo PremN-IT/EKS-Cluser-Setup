22a8073d2242a137d13da1fee9a476d4dd5d893d
Premkumar Nagaiah <138997281+PremN-IT@users.noreply.github.com>
2026-04-26 12:06:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "demo-terraform-eks-state-s3-bucket-test"
+    bucket         = "demo-terraform-eks-state-s3-bucket-test-workflow"
     key            = "terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "terraform-eks-state-locks"
     encrypt        = true
   }
 }
