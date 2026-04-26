b04ea4e4c51e1a8ead682515603c3dbb0efe4313
PremSaranya <nkprembe@gmail.com>
2026-04-26 12:35:45 +0530

    update module name

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "eks" {
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
-  vpc_id          = modules.vpc.vpc_id
-  subnet_ids      = modules.vpc.private_subnet_ids
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnet_ids
   node_groups     = var.node_groups
 }
